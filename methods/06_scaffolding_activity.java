84309ff42c0a0c4d568d9310b927bb6e2f8c994e
stacy  Goldstein <stacygoldstein@gmail.com>
2022-07-22 16:48:02 +0000

    scaffolding activity

---
+++ methods/06_scaffolding_activity.java
@@ -0,0 +1,36 @@
+public class Loops{
+    // THIS WILL NOT RUN UNLESS YOU RENAME THIS FILE Loops
+  
+    public static void main(String[] args) {
+    // 1. Nested looping
+
+      // make a loop that runs 10 times
+
+
+        // add a print statement to let people know the outer loop is running
+
+
+        // make a loop INSIDE the loop you made, run it 5 times
+        // if this loop seems to happen too few times, or if you get an error, talk it out with
+        // your group, and then check with me to see if you've figured out why
+
+          // add a message to let people know the inner loop is running
+
+    
+  	// 2. Looping & Conditionals
+    
+    
+    // expand and intigrate activites
+    // make a loop that runs 10 times
+
+      // add a message to let people know the outer loop is running
+
+      // if i is even
+        // then loop 10 times
+          // add a message to let people know the inner loop is running
+            // every other loop print "i and j are both even!"
+
+
+
+  }
+}
